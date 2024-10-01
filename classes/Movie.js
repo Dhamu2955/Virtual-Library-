a081008c3c1858fc2335614fa421dc12983ea28b
Dhamu2955 <Dharmendra.Pindolia@gmail.com>
2024-10-01 13:57:58 +0100

    finished project

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,21 @@
-// import the Media class:
+const Media = require("./Media")
 
-// create your Movie class:
+class Movie extends Media {
+    constructor(title, year, genre, director, duration ,rating) {
+        super(title, year, genre)
+        this.director = director
+        this.rating = rating
+        this.duration = duration
+    }
+    summary(){
+        return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`
+    }
+    static longestMovie(movies){
+        let temp = movies
+        temp.sort((a,b) => b.duration - a.duration)
+        return temp[0]
+    }
+}
 
 // don't change below
 module.exports = Movie;
--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,20 @@
-// import the Media class:
+const Media = require("./Media")
 
-// create your Music class:
+class Music extends Media{
+    constructor(title, year, genre, artist, length){
+        super(title, year, genre)
+        this.artist = artist
+        this.length = length
+    }
+    summary(){
+        return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`
+    }
+    static shortestAlbum(albums){
+        let temp = albums
+        temp.sort((a,b) => a.length - b.length)
+        return temp[0]
+    }
+}
 
 // don't change below
 module.exports = Music;
